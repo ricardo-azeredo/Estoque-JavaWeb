a67f39fd5a6ee6d88d966b5f40d35b29e3cc13a4
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2023-01-29 17:34:41 -0300

    create ListarProdutos list

--- src/java/br/com/estoque/DAO/ProdutoDAO.java
+++ src/java/br/com/estoque/DAO/ProdutoDAO.java
@@ -3,13 +3,15 @@ package br.com.estoque.DAO;
 import br.com.estoque.DTO.Produto;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ProdutoDAO {
     Connection conn;
     PreparedStatement pstm;
-    //ResultSet rs;
+    ResultSet rs;
+    ArrayList<Produto> lista = new ArrayList<>();
     
     public void CadastrarProduto(Produto produto) throws ClassNotFoundException{
         String sql = "INSERT INTO produtos (nome,valor,quantidade,min_quantidade) VALUES (?,?,?,?)";
@@ -17,10 +19,12 @@ public class ProdutoDAO {
         
         try {
            pstm = conn.prepareStatement(sql);
+           
            String nome = produto.getNome();
            double valor = produto.getValor();
            int quantidade = produto.getQuantidade();
            int minQuantidade = produto.getMinQuantidade();
+           
            pstm.setString(1,nome);
            pstm.setDouble(2, valor);
            pstm.setInt(3, quantidade);
@@ -34,5 +38,30 @@ public class ProdutoDAO {
             System.out.println("Não cadastrou o Produto. Erro: " + e);
         }
     
+    }
+    
+    public ArrayList<Produto> PesquisarProdutos() throws ClassNotFoundException{
+        String sql = "SELECT * FROM produtos";
+        conn = new ConexaoDAO().conexaoDB();
+        
+        try {
+            pstm = conn.prepareStatement(sql);
+            rs = pstm.executeQuery();
+            
+            while(rs.next()){
+                Produto produto = new Produto();
+                produto.setIdProduto(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getDouble("valor"));
+                produto.setQuantidade(rs.getInt("quantidade"));
+                produto.setMinQuantidade(rs.getInt("min_quantidade"));
+                lista.add(produto);
+            }
+            pstm.close();
+        }catch(SQLException e){
+            System.out.println("Não tem produtos. " + e);
+        }
+    
+        return lista;
     }
 }
